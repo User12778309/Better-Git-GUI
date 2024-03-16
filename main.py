ca2fe97f6945e6d9c364b96dbb623ec41277504f
User12778309 <sammy12778309@gmail.com>
2024-03-17 00:30:16 +0100

    add_button ( no function )

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-import tkinter
+# module/library
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+import webbrowser
+
+# create windows
+win = Tk()
+
+# set windows size
+win.geometry("400x600")
+win.maxsize(400,600)
+win.minsize(400,600)
+
+# transparent parametre
+
+# load image
+add_button_image = PhotoImage(file = "assets/add button.png",)
+
+# create add_button
+add_button = Button(image=add_button_image)
+
+# pack add_button
+add_button.pack(side=BOTTOM)
+
+# do loop
+win.mainloop()
