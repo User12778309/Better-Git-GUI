63b8a0d805c06124c336ba4de30c581e3ebf02f4
User12778309 <sammy12778309@gmail.com>
2024-05-10 12:05:50 +0200

    add option button frames

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 # module/library
 from tkinter import *
-from tkinter import filedialog
-from tkinter import messagebox
-import webbrowser
+from tkinter import ttk
 
-# create windows
+# set windows
 win = Tk()
 
-# set windows size
-win.geometry("400x600")
-win.maxsize(400,600)
-win.minsize(400,600)
+# windows geometry
+win.geometry("1080x720")
 
-# transparent parametre
-
-# load image
-add_button_image = PhotoImage(file = "assets/add button.png",)
-
-# create add_button
-add_button = Button(image=add_button_image)
-
-# pack add_button
-add_button.pack(side=BOTTOM)
+# set/pack option button frames
+option_button_frames = Frame(width=1080,height=32.5,bg="red")
+option_button_frames.pack()
 
 # do loop
 win.mainloop()
